a78038652030e2926d2d21e01c8b180caed09736
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-10-05 17:40:09 +0530

    feat: event-list-filter

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -1,22 +1,73 @@
+import { useState } from "react";
 import Box from "@mui/material/Box";
+import "../styles/eventList.css";
+import { eventList } from "../utils/utils";
+
+const categoryList = ["ALL", "NATIONAL", "MECH", "COMP", "IT", "ETC"];
 
 function EventList() {
-    return (
-        < >
-           <Box className="events">
-           <div className="d-flex row justify-content-center align-items-center m-0 p-0 my-5">
-                <hr className='w-15' />
-                <div className="fs-24 w-25 text-center">
-                    Events
-                </div>
-                <hr className='w-15' />
-            </div>
-            <Box className="event-container container">
-                EventList
-            </Box>
-            </Box> 
-        </>
-    )
+    const [selectedCategory, setSelectedCategory] = useState("ALL");
+  return (
+    <>
+      <Box sx={{mb: 3}} className="events" id="events">
+        <div className="d-flex row justify-content-center align-items-center m-0 p-0 my-5">
+          <hr className="w-15" />
+          <div className="fs-24 w-25 text-center">Events</div>
+          <hr className="w-15" />
+        </div>
+        <Box className="event-container container">
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexWrap: "wrap",
+              mb: 3
+            }}
+            className="event-controller container"
+          >
+            {categoryList.map((category, index) => {
+              return (
+                <Box
+                  sx={{ px: { xs: 2 }, py: { xs: 1 }, my: { xs: 1}, mx: { xs: 1} }}
+                  className={`event-pill ${selectedCategory == category ? 'active-pill': ''}`}
+                  key={index}
+                  onClick={() => setSelectedCategory(category)}
+                >
+                  {category}
+                </Box>
+              );
+            })}
+          </Box>
+          <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 1}} className="event-cards">
+            {
+              (selectedCategory == "ALL") ? (
+                eventList.map((event, index) => {
+                  return (
+                    <Box sx={{p: 2}} className="card" key={index}>
+                    <img src={event.image} alt="" className="event-thumbnail"/>
+                    <h3 className="event-title">{event.event}</h3>
+                    <p className="department-name">{event.department}</p>
+                  </Box>
+                  );
+                })
+              ) : (
+                eventList.filter(event => event.categories.includes(selectedCategory)).map((event, index) => {
+                  return (
+                    <Box sx={{p: 2}} className="card" key={index}>
+                    <img src={event.image} alt="" className="event-thumbnail"/>
+                    <h3 className="event-title">{event.event}</h3>
+                    <p className="department-name">{event.department}</p>
+                  </Box>
+                  );
+                })
+              )
+            }
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
-export default EventList
+export default EventList;
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -35,3 +35,110 @@ export const sponserList = [
         image: "/assets/images/sponsers/Sj_innovation.png"
     }
 ];
+
+export const eventList = [
+    {
+        event: "Decrypt",
+        department: "Computer Department",
+        categories: ["NATIONAL", "COMP"],
+        type: "Intra",
+        location: "Online",
+        date: "7th OCT",
+        time: "11 AM",
+        event_coordinators: [
+            {
+                name: "Vishrut Mangoli",
+                phone: "9766055610"
+            },
+            {
+                name: "K L Rithika",
+                phone: "8390629943"
+            }
+        ],
+        rules: {
+            Rules: [
+                "The contest shall consist of solving a series of challenges to reach the goal.",
+                "The contest will mainly deal with computer-based puzzles and tasks. Your knowledge of Computers, the internet (latest happenings around the world) and related technical and Non-technical stuff will be put to the test."
+            ],
+            round1_rules: [
+                "1 Student/participant.",
+                "25 MCQ/Questions to be answered in 60min.",
+                "The first one to successfully reach the goal, completing all challenges will qualify for round 2.",
+                "Internet access is allowed."
+            ],
+            round2_rules: [
+                "1 Student/participant.",
+                "15 Questions to be answered in 45min.",
+                "The first one to successfully reach the goal, completing all challenges will be declared the winner.- In case of a Tie there will be a Tie breaker Question.",
+                "Internet access is allowed.",
+                "Platform- Google Forms"
+            ],
+            FAQs: [
+                "1. Who can participate? <br/> Ans - Any student from Padre Conceicao College of Engineering irrespective of their department can participate. ",
+                "2. Is there a registration fee? <br/> Ans - No registration fee",
+                "3. What should you have? <br/> Ans - A PC/Laptop with internet access and preferably Google Chrome browser installed.",
+                "Decision taken by Co-Ordinators will be final in case any discrepancies!"
+            ]
+        },
+        prize: [
+            1500,
+            500
+        ],
+        form: "https://forms.gle/NpJYvGRYX2mfiPPv5",
+        poster: "../posters/COMP/decrypt.jpg",
+        rulebook: "../rulebook/COMP/decrypt.pdf",
+        image: '/assets/images/thumbnail/rpa.png'
+    },
+    {
+        event: "Game jam",
+        department: "Computer Department",
+        categories: ["NATIONAL", "IT"],
+        type: "Intra",
+        location: "Online",
+        date: "7th OCT",
+        time: "11 AM",
+        event_coordinators: [
+            {
+                name: "Vishrut Mangoli",
+                phone: "9766055610"
+            },
+            {
+                name: "K L Rithika",
+                phone: "8390629943"
+            }
+        ],
+        rules: {
+            Rules: [
+                "The contest shall consist of solving a series of challenges to reach the goal.",
+                "The contest will mainly deal with computer-based puzzles and tasks. Your knowledge of Computers, the internet (latest happenings around the world) and related technical and Non-technical stuff will be put to the test."
+            ],
+            round1_rules: [
+                "1 Student/participant.",
+                "25 MCQ/Questions to be answered in 60min.",
+                "The first one to successfully reach the goal, completing all challenges will qualify for round 2.",
+                "Internet access is allowed."
+            ],
+            round2_rules: [
+                "1 Student/participant.",
+                "15 Questions to be answered in 45min.",
+                "The first one to successfully reach the goal, completing all challenges will be declared the winner.- In case of a Tie there will be a Tie breaker Question.",
+                "Internet access is allowed.",
+                "Platform- Google Forms"
+            ],
+            FAQs: [
+                "1. Who can participate? <br/> Ans - Any student from Padre Conceicao College of Engineering irrespective of their department can participate. ",
+                "2. Is there a registration fee? <br/> Ans - No registration fee",
+                "3. What should you have? <br/> Ans - A PC/Laptop with internet access and preferably Google Chrome browser installed.",
+                "Decision taken by Co-Ordinators will be final in case any discrepancies!"
+            ]
+        },
+        prize: [
+            1500,
+            500
+        ],
+        form: "https://forms.gle/NpJYvGRYX2mfiPPv5",
+        poster: "../posters/COMP/decrypt.jpg",
+        rulebook: "../rulebook/COMP/decrypt.pdf",
+        image: '/assets/images/thumbnail/rpa.png'
+    }
+];
