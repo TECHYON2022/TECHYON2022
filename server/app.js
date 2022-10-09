a8f38a48cc3494aad2edfde1db6156b4c40c7fc9
MelittaPacheco <melittapacheco@gmail.com>
2022-10-09 10:48:48 +0000

    Melitta/feature added: Single event

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser"
+import connectDB from "./config/db.js";
+import router from "./routes/index.js"; 
 
 const app = express();
+
+// JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(router);
 
 app.get("/", (req, res) => {
   res.send("I'm Working!!");
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
+
+//routes goes here
 import express from "express";
 import singleParticipantRouter from "./singleParticipant.routes.js"
-import teamrouter from "./team.routes.js";
-const router=express.Router();
-
-
-router.use('/teamEvent',teamrouter)
+ const router= express.Router();
+  
 router.use("/SingleParticipant", singleParticipantRouter) ;
+
 
 export default router;
