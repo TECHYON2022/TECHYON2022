4dfb94a338e4299c10f7733e048f172dc5cba5e8
praTHM23 <cspcce2023@gmail.com>
2022-10-11 16:11:09 +0530

    feat: Added team Event

---
+++ server/controllers/team.controller.js
@@ -0,0 +1,59 @@
+
+import {Ideate , CodeCrunch} from '../models/team.model.js'
+import mongoose from 'mongoose'
+
+
+
+export const postEvent = async (req, res) => {
+
+    try {
+        console.log(req.body.eventName)
+        const eventName = req.body.eventName
+        switch (eventName) {
+            case 'Ideate':
+                const ideate = await Ideate.create(req.body)
+                await ideate.sendmail()
+                res.status(200).send(ideate)
+                break
+            case 'CodeCrunch':
+                const codecrunch = await CodeCrunch.create(req.body)
+                await codecrunch.sendmail()
+                res.status(200).send(codecrunch)
+                break
+
+        }
+    }
+    catch (err) {
+        res.status(422).send(err)
+    }
+
+}
+
+export const getEvent = async (req, res) => {
+
+
+    const eventName = req.body.eventName
+
+
+    try {
+        switch (eventName) {
+            case 'Ideate':
+                const ideate = await Ideate.find()
+                res.status(200).send(ideate)
+                break
+            case 'CodeCrunch':
+                const codecrunch = await CodeCrunch.find()
+                res.status(200).send(codecrunch)
+                break
+
+        }
+
+    } catch (err) {
+        res.status(422).send(err)
+    }
+
+}
+
+
+
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,1 +1,9 @@
-//routes goes here
+import  express  from "express";
+import teamrouter from "./team.routes.js";
+
+const router=express.Router();
+
+router.use('/teamEvent',teamrouter)
+
+
+export default router
