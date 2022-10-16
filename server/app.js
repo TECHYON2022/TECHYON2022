74a70f76f273e111670430251ad8e93b84d9829a
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-10-16 19:35:35 +0530

    chore: build

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use(express.static("public"));
 
 connectDB();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
