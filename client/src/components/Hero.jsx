563c520a8c068145fd2df2aa2a426b264c572c05
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-10-22 09:40:12 +0530

    refactor: ui

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import '../styles/hero.css';
 
 function Hero() {
     const [isMobile, setIsMobile] = useState(false);
+
+    const [text, count] = useTypewriter({
+        words: ["Here !!!!", "Here !!!!", "Here !!!!"],
+        loop: 0,
+        typeSpeed: 80,
+        deleteSpeed: 50 
+      })
+
     useEffect(() => {
         if (window) {
             if (window.matchMedia("(max-width: 500px)").matches) {
@@ -11,13 +20,13 @@ function Hero() {
               setIsMobile(false);
             }
         } 
-    })
+    }, [])
     return (
         <>
             <div className={`${isMobile ? '' : 'vh-100'} hero-bg d-flex row align-items-center m-0 p-0 w-100 mb-4`}>
                 <div className={`${isMobile ? 'col-12 mt-5':'col-6 row justify-content-center'} p-0 m-0`}>
                     <div className={`${isMobile ? 'col-12 p-4':'col-6'} p-0 m-0`}>
-                        <h1 className={`${isMobile ? 'fs-38' : 'fs-48'} hero-text`}>Our Very Own <span className="color-accent">Tech Event</span> Is Here !!!!</h1>
+                        <h1 className={`${isMobile ? 'fs-38' : 'fs-48'} hero-text`}>Our Very Own <span className="color-accent">Tech Event</span> Is <span className="type">{text}</span><Cursor/></h1>
                         <h6 className={`${isMobile ? 'fs-18' : 'fs-28'} hero-date mt-4`}>27th OCT - 29th OCT</h6>
                         <button className='btn btn-custom mt-4 py-2 px-3'>Schedule</button>
                     </div>
